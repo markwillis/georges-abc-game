c6ef1b4021577a76b5793a6c05ea220e3dd7b1b7
Mark <mark@webflow.com>
2020-11-18 09:12:48 +0000

    break  out components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import Star from './components/star/Star';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+	green: {
+		backgroundImage:
+			'linear-gradient(45deg, hsla(108, 86.67%, 46.79%, 1.00), hsla(94, 100.00%, 11.67%, 1.00))',
+		color: 'hsla(0, 0.00%, 100.00%, 1.00)',
+	},
+	red: {
+		backgroundImage:
+			'linear-gradient(45deg, hsla(0, 86.67%, 46.79%, 1.00), hsla(94, 100.00%, 11.67%, 1.00))',
+		color: 'hsla(0, 0.00%, 100.00%, 1.00)',
+	},
+};
+
+const Letter = () => {
+	const [letter, setLetter] = useState('g');
+	const [keyPress, setKeyPress] = useState('');
+	const [color, setColor] = useState('');
+	const [stars, setStars] = useState(0);
+
+	const alphabet = [
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+	];
+
+	const getLetter = () => {
+		const randNum = Math.floor(Math.random() * 26);
+		setLetter(alphabet[randNum]);
+	};
+
+	useEffect(() => {
+		window.addEventListener('keydown', (event) => {
+			setKeyPress(event.key);
+		});
+	}, []);
+
+	useEffect(() => {
+		if (keyPress === letter) {
+			setTimeout(() => {
+				getLetter();
+				setColor('');
+			}, 1500);
+			setColor(styles.green.backgroundImage);
+			setStars(stars + 1);
+		} else {
+			setTimeout(() => {
+				setColor('');
+			}, 300);
+			setColor(styles.red.backgroundImage);
+		}
+	}, [keyPress]);
+
+	return (
+		<>
+			<div className={`letter-wrap`} style={{ backgroundImage: color }}>
+				<h1 className="big-letter">{letter.toUpperCase()}</h1>
+			</div>
+			<div className="star-wrapper">
+				{stars
+					? [...Array(stars)].map((star) => {
+							return <Star key={star} />;
+					  })
+					: null}
+			</div>
+		</>
+	);
+};
+
+export default function App() {
+	return (
+		<div className="App">
+			<Letter />
+		</div>
+	);
 }
-
-export default App;
---
+++ src/components/star/Star.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import star from '../../images/star.png';
+import './star.css';
+
+const Star = () => {
+	return <img src={star} alt="star" width="60" />;
+};
+
+export default Star;
